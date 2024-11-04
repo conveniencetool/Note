abe0599cf40c0ebe6acc19e320f35597349083ab
tool <tokumei24yayayu@gmail.com>
2024-11-04 11:54:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,5 @@ function addNoteToDOM(noteText) {
     };
 
     noteDiv.appendChild(deleteBtn);
-    notesContainer.insertBefore(noteDiv, notesContainer.firstChild); // 上に追加する代わりに下に追加
+    notesContainer.appendChild(noteDiv); // 下に追加
 }
